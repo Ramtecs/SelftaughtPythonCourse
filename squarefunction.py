70526e56a2d93d8fbfd1b316b4281b120d0fb495
Ramtecs <53129959+Ramtecs@users.noreply.github.com>
2019-07-27 13:38:57 -0700

    Add files via upload

---
+++ squarefunction.py
@@ -0,0 +1,16 @@
+_author_ = 'Ramses Murillo'
+'''/import datetime library
+import datetime
+Print("Creation date is: ", datetime.datetime.now())
+'''
+import datetime
+x = datetime.datetime.now()
+print("File created",x)
+
+
+def sqr(x):
+    x=int(x)
+    return x**2
+results =sqr(2)
+print(results)
+
