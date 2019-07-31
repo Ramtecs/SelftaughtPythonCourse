ed61a9b6900d8b5c656952814e32838ef3610615
Ramtecs <53129959+Ramtecs@users.noreply.github.com>
2019-07-30 22:16:51 -0700

    Create containers.py

---
+++ containers.py
@@ -0,0 +1,23 @@
+author_ = 'Ramses Murillo'
+'''/import datetime library
+import datetime
+Print("Creation date is: ", datetime.datetime.now())
+'''
+import datetime
+
+todaysdate = datetime.datetime.now()
+print("File run", todaysdate)
+
+
+
+
+#1. Create a list of tuples, with each tuple containing the longitude and latitude of somewhere you've lived or visited.
+WhereIlived ={"San jose":"Costa Rica","Vancouver, WA": "USA"}
+
+Location1=["San jose, costa rica","9.928069, -84.090729"]
+Location2=["Bellingham, Washington","48.754902, -122.478119"]
+
+Locations=["9.928069, -84.090729","48.754902, -122.478119"]
+print("I have lived in ",Location1[0],"with coordenates: ",Locations[0])
+print("I have lived in ",Location1[1],"with coordenates: ",Locations[1])
+
