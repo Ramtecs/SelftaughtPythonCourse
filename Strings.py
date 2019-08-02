b08a6a49b2fa170464d376bcd2d34e9979b90df4
Ramtecs <53129959+Ramtecs@users.noreply.github.com>
2019-08-02 15:33:21 -0700

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,359 @@
+_author_ = 'Ramses Murillo'
+'''/import datetime library
+import datetime
+Print("Creation date is: ", datetime.datetime.now())
+'''
+import datetime
+
+todaysdate = datetime.datetime.now()
+print("File run", todaysdate)
+S="  today"
+S=S.strip()
+n1= "abc"
+y= "today"
+adj =" too"
+n2 ="other"
+v="holl"
+r="""The {} the {} {}""".format(n1,v,adj,n2)
+print(r)
+'''
+1.Print every character in the string "Camus".
+2.Write a program that collects two strings from a user, inserts them into the string "Yesterday I wrote a [response_one].
+ I sent it to [response_ two]!" and prints a new string.
+ 
+ 
+ collect1 =input("please enter a sentence")
+ collect2 =input("please enter a sentence")
+ 
+ String= "yesterday I wrote a " + collect1 + " I sent it to " + Collect2
+ print(string)
+ 
+3.Use a method to make the string "aldous Huxley was born in 1894." grammatically correct by capitalizing the first letter in the sentence.
+string = "aldous Huxley was born in 1894.".
+4.Take the string "Where now? Who now? When now?" and call a method that returns a list that looks like: ["Where now?", "Who now?", "When now?"].
+String - "Where now? Who now? When now?"
+String= String.split("?")
+5.Replace every instance of "s" in "A screaming comes across the sky." with a dollar sign.
+string ="A screaming comes across the sky."
+string = "A screaming comes across the sky."
+string =string.replace("s","$")
+print(string)
+
+6.Use a method to find the first index of the character "m" in the string "Hemingway".
+
+x= 'Hemingway'
+for x in range(0, 8):
+if m==str(x):
+print("The index for m is, x)
+
+
+
+7.Create the string "three three three" using concatenation, and then again using multiplication.
+Three ="Three" * 3
+print(Three)
+
+A= "Three"
+B= "Three"
+C= "Three"
+print(A+B+C)
+
+#1
+str = "Camus"
+
+for x in range(0, 4):
+    print(str[x])
+
+string = "A screaming comes across the sky."
+Length = len(string)
+print(Length)
+#for x in range(0, (Length - 1)):
+    #if (string(x)=="s"):
+        # string.format()
+    #print(string[x])
+
+string = "A screaming comes across the sky."
+string =string.replace("s","$")
+print(string)
+'''
+string = "Where now? Who now? When now?".replace("?","?")
+string =string.replace("W",'"W')
+string =string.replace("?",'?"')
+string =string.replace('" "','" , "')
+string =string.split(",")
+#r=="""The {} the {} {}""".format(n1,v,adj,n2)
+
+"today is ok. Tomorrow is not".split(".")
+#print(string)
+
+print(string)
+
+string = "aldous Huxley was born in 1894.".capitalize()
+print(string)
+
+
+####################
+
+_author_ = 'Ramses Murillo'
+'''/import datetime library
+import datetime
+Print("Creation date is: ", datetime.datetime.now())
+'''
+import datetime
+
+todaysdate = datetime.datetime.now()
+print("File run", todaysdate)
+S = "  today"
+S = S.strip()
+n1 = "abc"
+y = "today"
+adj = " too"
+n2 = "other"
+v = "holl"
+r = """The {} the {} {}""".format(n1, v, adj, n2)
+print(r)
+
+#1.Print every character in the string "Camus".
+
+
+str = "Camus"
+
+for x in range(0, 4):
+    print(str[x])
+
+
+
+#2.Write a program that collects two strings from a user, inserts them into the string "Yesterday I wrote a [response_one]. I sent it to [response_ two]!" and prints a new string.
+
+
+
+collect1 =input("please enter a sentence")
+collect2 =input("please enter a sentence")
+
+String= "yesterday I wrote a " + collect1 + " I sent it to " + collect2
+print(String)
+
+#3.Use a method to make the string "aldous Huxley was born in 1894." grammatically correct by capitalizing the first letter in the sentence.
+
+
+string = "aldous Huxley was born in 1894.".capitalize()
+print(string)
+
+#4.Take the string "Where now? Who now? When now?" and call a method that returns a list that looks like: ["Where now?", "Who now?", "When now?"].
+
+
+String1 = "Where now? Who now? When now?"
+String1 =String1.replace("?","?.")
+String1= String1.split('.')
+
+print("Version I :",String1)
+#print(String1[2])
+
+string = "Where now? Who now? When now?".replace("?","?")
+string =string.replace("W",'"W')
+string =string.replace("?",'?"')
+string =string.replace('" "','" , "')
+print("version II",string)
+
+#5.Replace every instance of "s" in "A screaming comes across the sky." with a dollar sign.
+string ="A screaming comes across the sky."
+string = "A screaming comes across the sky."
+string =string.replace("s","$")
+print(string)
+
+#6.Use a method to find the first index of the character "m" in the string "Hemingway".
+str = "Hemingway"
+
+for x in range(0, 4):
+    if 'm' == str[x]:
+        print('The index for M is ', x)
+
+
+#7.Create the string "three three three" using concatenation, and then again using multiplication.
+Three ="Three" * 3
+print(Three)
+
+A= "Three"
+B= "Three"
+C= "Three"
+print(A+B+C)
+'''
+output:
+_author_ = 'Ramses Murillo'
+'''/import datetime library
+import datetime
+Print("Creation date is: ", datetime.datetime.now())
+'''
+import datetime
+
+todaysdate = datetime.datetime.now()
+print("File run", todaysdate)
+S="  today"
+S=S.strip()
+n1= "abc"
+y= "today"
+adj =" too"
+n2 ="other"
+v="holl"
+r="""The {} the {} {}""".format(n1,v,adj,n2)
+print(r)
+'''
+1.Print every character in the string "Camus".
+2.Write a program that collects two strings from a user, inserts them into the string "Yesterday I wrote a [response_one].
+ I sent it to [response_ two]!" and prints a new string.
+ 
+ 
+ collect1 =input("please enter a sentence")
+ collect2 =input("please enter a sentence")
+ 
+ String= "yesterday I wrote a " + collect1 + " I sent it to " + Collect2
+ print(string)
+ 
+3.Use a method to make the string "aldous Huxley was born in 1894." grammatically correct by capitalizing the first letter in the sentence.
+string = "aldous Huxley was born in 1894.".
+4.Take the string "Where now? Who now? When now?" and call a method that returns a list that looks like: ["Where now?", "Who now?", "When now?"].
+String - "Where now? Who now? When now?"
+String= String.split("?")
+5.Replace every instance of "s" in "A screaming comes across the sky." with a dollar sign.
+string ="A screaming comes across the sky."
+string = "A screaming comes across the sky."
+string =string.replace("s","$")
+print(string)
+
+6.Use a method to find the first index of the character "m" in the string "Hemingway".
+
+x= 'Hemingway'
+for x in range(0, 8):
+if m==str(x):
+print("The index for m is, x)
+
+
+
+7.Create the string "three three three" using concatenation, and then again using multiplication.
+Three ="Three" * 3
+print(Three)
+
+A= "Three"
+B= "Three"
+C= "Three"
+print(A+B+C)
+
+#1
+str = "Camus"
+
+for x in range(0, 4):
+    print(str[x])
+
+string = "A screaming comes across the sky."
+Length = len(string)
+print(Length)
+#for x in range(0, (Length - 1)):
+    #if (string(x)=="s"):
+        # string.format()
+    #print(string[x])
+
+string = "A screaming comes across the sky."
+string =string.replace("s","$")
+print(string)
+'''
+string = "Where now? Who now? When now?".replace("?","?")
+string =string.replace("W",'"W')
+string =string.replace("?",'?"')
+string =string.replace('" "','" , "')
+string =string.split(",")
+#r=="""The {} the {} {}""".format(n1,v,adj,n2)
+
+"today is ok. Tomorrow is not".split(".")
+#print(string)
+
+print(string)
+
+string = "aldous Huxley was born in 1894.".capitalize()
+print(string)
+
+
+####################
+
+_author_ = 'Ramses Murillo'
+'''/import datetime library
+import datetime
+Print("Creation date is: ", datetime.datetime.now())
+'''
+import datetime
+
+todaysdate = datetime.datetime.now()
+print("File run", todaysdate)
+S = "  today"
+S = S.strip()
+n1 = "abc"
+y = "today"
+adj = " too"
+n2 = "other"
+v = "holl"
+r = """The {} the {} {}""".format(n1, v, adj, n2)
+print(r)
+
+#1.Print every character in the string "Camus".
+
+
+str = "Camus"
+
+for x in range(0, 4):
+    print(str[x])
+
+
+
+#2.Write a program that collects two strings from a user, inserts them into the string "Yesterday I wrote a [response_one]. I sent it to [response_ two]!" and prints a new string.
+
+
+
+collect1 =input("please enter a sentence")
+collect2 =input("please enter a sentence")
+
+String= "yesterday I wrote a " + collect1 + " I sent it to " + collect2
+print(String)
+
+#3.Use a method to make the string "aldous Huxley was born in 1894." grammatically correct by capitalizing the first letter in the sentence.
+
+
+string = "aldous Huxley was born in 1894.".capitalize()
+print(string)
+
+#4.Take the string "Where now? Who now? When now?" and call a method that returns a list that looks like: ["Where now?", "Who now?", "When now?"].
+
+
+String1 = "Where now? Who now? When now?"
+String1 =String1.replace("?","?.")
+String1= String1.split('.')
+
+print("Version I :",String1)
+#print(String1[2])
+
+string = "Where now? Who now? When now?".replace("?","?")
+string =string.replace("W",'"W')
+string =string.replace("?",'?"')
+string =string.replace('" "','" , "')
+print("version II",string)
+
+#5.Replace every instance of "s" in "A screaming comes across the sky." with a dollar sign.
+string ="A screaming comes across the sky."
+string = "A screaming comes across the sky."
+string =string.replace("s","$")
+print(string)
+
+#6.Use a method to find the first index of the character "m" in the string "Hemingway".
+str = "Hemingway"
+
+for x in range(0, 4):
+    if 'm' == str[x]:
+        print('The index for M is ', x)
+
+
+#7.Create the string "three three three" using concatenation, and then again using multiplication.
+Three ="Three" * 3
+print(Three)
+
+A= "Three"
+B= "Three"
+C= "Three"
+print(A+B+C)
+'''
